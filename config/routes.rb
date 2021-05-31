bdb690f3bf0a686007ed0a99b9f9a6e69cc7ea17
AlbanianSilka <andrii.khavriuta@gmail.com>
2021-05-31 16:00:16 +0300

    Updated routes and schema

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'pages/home'
+  devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   resources :posts do
@@ -6,4 +8,5 @@ Rails.application.routes.draw do
   end
 
   root "posts#index"
+  # root to: "home#index"
 end
