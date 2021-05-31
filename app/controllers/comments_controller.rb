c66403c7f06d6857db0974c2548feadf25317a91
AlbanianSilka <andrii.khavriuta@gmail.com>
2021-05-31 19:42:07 +0300

    Now only comment author can delete them

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,8 +2,7 @@ class CommentsController < ApplicationController
 
   def create
     @post = Post.find(params[:post_id])
-    # @comment = @post.comments.create(params[:comment]).permit(:name, :comment)
-    @comment = @post.comments.create(params[:comment].permit(:name, :comment))
+    @comment = @post.comments.create(params[:comment].permit(:name, :comment, :user_id))
     redirect_to post_path(@post)
   end
 
@@ -13,6 +12,5 @@ class CommentsController < ApplicationController
     @comment.destroy
     redirect_to post_path(@post)
   end
-
 
 end
