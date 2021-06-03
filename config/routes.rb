701141505edadcc7a217e40b97a03427446e4f3c
AlbanianSilka <andrii.khavriuta@gmail.com>
2021-06-03 15:02:00 +0300

    Added new routes for comments and posts

--- config/routes.rb
+++ config/routes.rb
@@ -4,10 +4,14 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   resources :posts do
+    member do
+      put :like, to:'posts#upvote'
+      put :dislike, to:'posts#downvote'
+    end
     resources :comments do
       member do
         put :like, to:'comments#upvote'
-        put :like, to:'comments#downvote'
+        put :dislike, to:'comments#downvote'
       end
     end
     delete :destroy_attachment
