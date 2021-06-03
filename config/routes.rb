f01f9fda7df82cc37707d49fff98ac617f2b705f
AlbanianSilka <andrii.khavriuta@gmail.com>
2021-06-03 11:53:25 +0300

    Added new def for posts to upload files

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,13 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   resources :posts do
-    resources :comments
+    resources :comments do
+      member do
+        put :like, to:'comments#upvote'
+        put :like, to:'comments#downvote'
+      end
+    end
+    delete :destroy_attachment
   end
 
   root "posts#index"
