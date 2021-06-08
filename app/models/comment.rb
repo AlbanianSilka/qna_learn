90193141534ad63f4958cc1b279da77d5317088b
AlbanianSilka <andrii.khavriuta@gmail.com>
2021-06-08 12:20:10 +0300

    Added association for answers and comments

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,7 @@
 class Comment < ApplicationRecord
   belongs_to :post
+  has_many :answers
   has_one_attached :file
+  after_create_commit { RenderCommentJob.perform_later self }
   acts_as_votable
 end
