49e4434eb27770c426616286ea9c5173d69c4d3a
AlbanianSilka <andrii.khavriuta@gmail.com>
2021-06-17 18:12:40 +0300

    Changed best comment default to false

--- db/migrate/20210529130604_create_comments.rb
+++ db/migrate/20210529130604_create_comments.rb
@@ -3,7 +3,7 @@ class CreateComments < ActiveRecord::Migration[6.1]
     create_table :comments do |t|
       t.string :name
       t.text :comment
-      t.boolean :best
+      t.boolean :best, default: false
 
       t.timestamps
     end
