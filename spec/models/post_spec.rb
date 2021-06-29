05a71f3db61457888be6f61b68494ac9bf795b6f
AlbanianSilka <andrii.khavriuta@gmail.com>
2021-06-29 11:07:35 +0300

    Updated post spec

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -3,4 +3,5 @@ require 'rails_helper'
 RSpec.describe Post, type: :model do
   it { should validate_presence_of :content }
   it { should validate_presence_of :title }
+  it { should have_many(:comments).dependent(:destroy) }
 end
