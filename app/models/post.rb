166aeaea986dd1eda81aaadcbf93fc0cff0d4cb7
AlbanianSilka <andrii.khavriuta@gmail.com>
2021-06-29 11:07:16 +0300

    Validation fix for post model

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,7 +2,7 @@ class Post < ApplicationRecord
   validates :title, :content, presence: true
   has_many_attached :file
   has_many :comments
-  has_many :subscriptions, dependent: :delete_all
-  belongs_to :user
+  # has_many :subscriptions, dependent: :delete_all
+  belongs_to :user, class_name: "User", optional: true
   acts_as_votable
 end
